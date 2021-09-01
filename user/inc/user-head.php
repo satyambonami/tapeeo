1a3341e59e75b14d170fad7c5a6bad1c24882169
satyambonami <satyambharadwaj@bonamisoftware.com>
2021-09-01 18:07:56 +0530

    user head

--- user/inc/user-head.php
+++ user/inc/user-head.php
@@ -1,8 +1,4 @@
 <link href="css/side-nav.css" rel="stylesheet" />
 <link href="css/dashboard.css" rel="stylesheet" />
 <link href="css/cart.css" rel="stylesheet" />
-<<<<<<< HEAD
-
-=======
 <link href="css/order.css" rel="stylesheet" />
->>>>>>> 64b8dc6e2f2fa212f44bc291469eb1cb9d0aef45
